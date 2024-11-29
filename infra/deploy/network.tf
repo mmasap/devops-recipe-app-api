558dcb27528fa4945e81aedc57873bd790c1ed48
mmasap <miura.worker@gmail.com>
2024-11-29 13:36:12 +0900

    added private subnets

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -66,3 +66,23 @@ resource "aws_route" "public_internet_access_c" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+  tags = {
+    Name = "${local.prefix}-private-a"
+  }
+
+}
+
+resource "aws_subnet" "private_c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}c"
+  tags = {
+    Name = "${local.prefix}-private-c"
+  }
+}
+
