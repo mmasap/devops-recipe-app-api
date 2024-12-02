b9a47182af7a3a8e95b2e6e30974cf25221b6ec0
mmasap <miura.worker@gmail.com>
2024-12-02 13:16:00 +0900

    added security group for ecs

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -142,3 +142,34 @@ resource "aws_ecs_task_definition" "api" {
     cpu_architecture        = "X86_64"
   }
 }
+
+
+resource "aws_security_group" "ecs_service" {
+  description = "Access rules for the ECS service"
+  name        = "${local.prefix}-ecs-service"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_c.cidr_block
+    ]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
